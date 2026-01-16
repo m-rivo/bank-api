9920b56e24bd4d7be5a023b6d649c08f90b69b2a
Marycarmen <marycarmen.rivo@gmail.com>
2026-01-16 14:35:47 -0600

    Creación de constantes globales

---
+++ BankSystem.Core/Settings/ConstantesGlobales.cs
@@ -0,0 +1,7 @@
+﻿namespace BankSystem.Core.Settings;
+
+public static class ConstantesGlobales
+{
+    public const decimal TasaInteresMensual = 0.01m; // 1%
+    public const decimal MontoMinimoSaldoInicial = 1000m;
+}
