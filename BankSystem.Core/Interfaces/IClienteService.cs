81c026c2888f8c5d4f3d9a9711ee3d167ff8d3fa
Marycarmen <marycarmen.rivo@gmail.com>
2026-01-15 23:04:27 -0600

    Creación de interfaz de ClienteService

---
+++ BankSystem.Core/Interfaces/IClienteService.cs
@@ -0,0 +1,10 @@
+﻿using BankSystem.Core.Entities;
+
+namespace BankSystem.Core.Interfaces
+{
+    public interface IClienteService
+    {
+        Task<Cliente> CrearClienteAsync(Cliente cliente);
+        Task<Cliente?> ObtenerPorIdAsync(int id);
+    }
+}
