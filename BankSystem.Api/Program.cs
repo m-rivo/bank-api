573a4e32ac26170c24a682101accc9519456b540
Marycarmen <marycarmen.rivo@gmail.com>
2026-01-15 23:01:16 -0600

    Actualización de Progrem.cs para utilizar SQLite, Swagger y obtener enums como strings

--- BankSystem.Api/Program.cs
+++ BankSystem.Api/Program.cs
@@ -1,13 +1,34 @@
+using System.Text.Json.Serialization;
+using BankSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// 1. Configuración de Servicios (Dependency Injection)
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Esto hace que los Enums se vean como texto "M"/"F" en la API
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+// Configuración de Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configuración de SQLite
+builder.Services.AddDbContext<BankDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Servicios
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 2. Configuración de Pipeline (Middleware)
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,30 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
