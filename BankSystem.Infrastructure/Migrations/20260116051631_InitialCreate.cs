ec218417d1e649abb76c628672beb3c20cc4b1c2
Marycarmen <marycarmen.rivo@gmail.com>
2026-01-16 09:04:15 -0600

    Base de datos generada

---
+++ BankSystem.Infrastructure/Migrations/20260116051631_InitialCreate.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Sexo = table.Column<string>(type: "TEXT", nullable: false),
+                    Ingresos = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cuentas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NumeroCuenta = table.Column<string>(type: "TEXT", nullable: false),
+                    Saldo = table.Column<double>(type: "REAL", nullable: false),
+                    IdCliente = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuentas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cuentas_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdCuenta = table.Column<int>(type: "INTEGER", nullable: false),
+                    TipoTransaccion = table.Column<string>(type: "TEXT", nullable: false),
+                    Monto = table.Column<double>(type: "REAL", nullable: false),
+                    SaldoPostTransaccion = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CuentaId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacciones_Cuentas_CuentaId",
+                        column: x => x.CuentaId,
+                        principalTable: "Cuentas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cuentas_ClienteId",
+                table: "Cuentas",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cuentas_NumeroCuenta",
+                table: "Cuentas",
+                column: "NumeroCuenta",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacciones_CuentaId",
+                table: "Transacciones",
+                column: "CuentaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacciones");
+
+            migrationBuilder.DropTable(
+                name: "Cuentas");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
