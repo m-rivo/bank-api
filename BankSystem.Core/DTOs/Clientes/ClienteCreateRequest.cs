a992e3bb2b2fd931d3bd1a9aae698a199c1e7179
Marycarmen <marycarmen.rivo@gmail.com>
2026-01-15 23:02:37 -0600

    Creación de Dtos ClienteCreateRequest y ClienteResponse

---
+++ BankSystem.Core/DTOs/Clientes/ClienteCreateRequest.cs
@@ -0,0 +1,9 @@
+﻿using BankSystem.Core.Enums;
+namespace BankSystem.Core.DTOs;
+
+public record ClienteCreateRequest(
+    string Nombre,
+    DateTime FechaNacimiento,
+    Sexo Sexo,
+    decimal Ingresos
+);
---
+++ BankSystem.Core/DTOs/Clientes/ClienteResponse.cs
@@ -0,0 +1,10 @@
+﻿using BankSystem.Core.Enums;
+namespace BankSystem.Core.DTOs.Clientes;
+
+public record ClienteResponse(
+    int Id,
+    string Nombre,
+    DateTime FechaNacimiento,
+    Sexo Sexo,
+    decimal Ingresos
+);
